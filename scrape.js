bb3fed5a5a3d6b573b1558e5e85e041ae0b2d1a2
bterbs <brittanyterbush@gmail.com>
2017-10-26 11:12:17 -0700

    added jsdoc module to project

--- scrape.js
+++ scrape.js
@@ -4,17 +4,17 @@
 var fetch = require('node-fetch');
 var cheerio = require('cheerio');
 
-const BASE_URL = 'https://amazon.com/';
+const BASE_URL = 'https://www.google.com/';
 
 /* gets HTML data from page using node-fetch, parses with cheerio */
 
 const getData = (itemNumber) => {
   fetch(`${BASE_URL}/${itemNumber}`)
     .then(function(res) {
-        return res.text();
+        console.log(res.text());
     }).then(function(body) {
         const $ = cheerio.load(body);
-        console.log(`'title:', ${title}.text()`);
+        console.log($);
     });
-    .catch(console.error)
+  //  .catch(console.error)
 }
