6cae4e7ab4ee2c8d46852df5828ab3ba8562feb0
Bonnie Schulkin <bonnie@learnersguild.com>
2017-10-27 10:32:47 -0700

    Made coder cleaner and more maintainable.

--- movie-search.js
+++ movie-search.js
@@ -1,19 +1,31 @@
 const request = require('request');
 const cheerio = require('cheerio');
+const buildUrl = require('build-url');
 
-const searchTerm = process.argv.slice(2);
+const searchTerm = process.argv.slice(2).join(' ');
+const IMDBserver = 'http://www.imdb.com'
+const IMDBpath = 'find'
+const queryParams = {
+  ref_: 'nv_sr_fn',
+  s: 'all',
+  q: searchTerm }
 
 const options = {
-  url: 'http://www.imdb.com/find?ref_=nv_sr_fn&q=' + searchTerm + '&s=all',
+  url: buildUrl(IMDBserver, { path: IMDBpath, queryParams }),
   method: 'GET',
 }
 
-request(options, function (error, response, html) {
-  if (!error && response.statusCode == 200) {
-    var $ = cheerio.load(html);
-    $('td.result_text').each(function(i, element){
-    var a = $(this);
-    console.log(a.text());
+request(options, (error, response, html) => {
+  if (error) {
+    console.error(`An error occurred retrieving the html: ${error.message}. Exiting`)
+    process.exit(1)
+  }
+  if (response.statusCode !== 200) {
+    console.error(`got non-200 response status: ${response.statusCode}. Exiting`)
+    process.exit(1)
+  }
+  const $ = cheerio.load(html);
+  $('td.result_text').each((i, element) => {
+    console.log($(element).text());
   });
-    };
 });
