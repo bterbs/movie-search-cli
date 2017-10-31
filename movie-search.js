3dd1aeea725a8a3d2b2dadf16054de03b7675721
B Terbush <brittanyterbush@gmail.com>
2017-10-30 17:50:24 -0700

    refactored code per Bonnies feedback on Friday

--- movie-search.js
+++ movie-search.js
@@ -1,19 +1,31 @@
 const request = require('request');
 const cheerio = require('cheerio');
+const buildUrl = require('build-url');
+const rp = require('request-promise');
 
-const searchTerm = process.argv.slice(2);
+
+const searchTerm = process.argv.slice(2).join('');
+const imdbServer = 'http://www.imdb.com/';
+const imdbPath = 'find'
+const queryParams = { ref_: 'nv_sr_fn',   s: 'all', q: searchTerm }
 
 const options = {
-  url: 'http://www.imdb.com/find?ref_=nv_sr_fn&q=' + searchTerm + '&s=all',
+  url: buildUrl(imdbServer, { path: imdbPath, queryParams}),
   method: 'GET',
 }
 
 request(options, function (error, response, html) {
-  if (!error && response.statusCode == 200) {
-    var $ = cheerio.load(html);
-    $('td.result_text').each(function(i, element){
+  if (error) {
+    console.error('Error occurred during html request!');
+    process.exit(1)
+  }
+  if (response.statusCode !== 200) {
+    console.error(`got non-200 response status: ${response.statusCode}`)
+    process.exit(1)
+  }
+  const $ = cheerio.load(html);
+  $('td.result_text').each((i, element) => {
     var a = $(this);
     console.log(a.text());
   });
-    };
 });
